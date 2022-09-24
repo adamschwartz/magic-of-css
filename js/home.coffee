af736b43950efad96afe1b36620a67e69b9affbd
Adam Schwartz <adam.flynn.schwartz@gmail.com>
2022-09-24 09:35:12 -0700

    more home animation improvements

--- js/home.coffee
+++ js/home.coffee
@@ -1,25 +1,21 @@
-home = {}
+document.documentElement.setAttribute? 'data-user-agent', navigator.userAgent
 
-home.init = ->
-    document.documentElement.setAttribute? 'data-user-agent', navigator.userAgent
+firedScroll = false
+window.addEventListener 'scroll', ->
+    return if firedScroll
+    firedScroll = true
+    document.body.className += ' window-has-been-scrolled'
 
-    firedScroll = false
-    window.addEventListener 'scroll', ->
-        return if firedScroll
-        firedScroll = true
-        document.body.className += ' window-has-been-scrolled'
+if localStorage.hasBeenHereBefore is true
+    document.body.className += ' has-been-here-before'
+else
+    localStorage.hasBeenHereBefore = true
 
-    if localStorage.hasBeenHereBefore is true
-        document.body.className += ' has-been-here-before'
-    else
-        localStorage.hasBeenHereBefore = true
+referrerHostname = document.referrer.match(/:\/\/(.[^/]+)/)?[1].split(':')[0]
 
-    referrerHostname = document.referrer.match(/:\/\/(.[^/]+)/)?[1].split(':')[0]
-
+unless location.search.match(/animate/)?
     if document.location.hostname is referrerHostname or localStorage.hasBeenHereBefore is true or location.search.match(/fast/)?
         document.body.className += ' has-been-here-before'
 
-    document.body.className += ' trigger-pageload-animations'
-    setTimeout (-> document.body.className += ' trigger-pageload-animations-3s'), 3000
-
-window.home = home
+document.body.className += ' trigger-pageload-animations'
+setTimeout (-> document.body.className += ' trigger-pageload-animations-3s'), 3000
--- js/home.js
+++ js/home.js
@@ -1,36 +1,38 @@
 (function() {
-  var home;
+  var base, firedScroll, ref, referrerHostname;
 
-  home = {};
+  if (typeof (base = document.documentElement).setAttribute === "function") {
+    base.setAttribute('data-user-agent', navigator.userAgent);
+  }
 
-  home.init = function() {
-    var base, firedScroll, ref, referrerHostname;
-    if (typeof (base = document.documentElement).setAttribute === "function") {
-      base.setAttribute('data-user-agent', navigator.userAgent);
+  firedScroll = false;
+
+  window.addEventListener('scroll', function() {
+    if (firedScroll) {
+      return;
     }
-    firedScroll = false;
-    window.addEventListener('scroll', function() {
-      if (firedScroll) {
-        return;
-      }
-      firedScroll = true;
-      return document.body.className += ' window-has-been-scrolled';
-    });
-    if (localStorage.hasBeenHereBefore === true) {
-      document.body.className += ' has-been-here-before';
-    } else {
-      localStorage.hasBeenHereBefore = true;
-    }
-    referrerHostname = (ref = document.referrer.match(/:\/\/(.[^\/]+)/)) != null ? ref[1].split(':')[0] : void 0;
+    firedScroll = true;
+    return document.body.className += ' window-has-been-scrolled';
+  });
+
+  if (localStorage.hasBeenHereBefore === true) {
+    document.body.className += ' has-been-here-before';
+  } else {
+    localStorage.hasBeenHereBefore = true;
+  }
+
+  referrerHostname = (ref = document.referrer.match(/:\/\/(.[^\/]+)/)) != null ? ref[1].split(':')[0] : void 0;
+
+  if (location.search.match(/animate/) == null) {
     if (document.location.hostname === referrerHostname || localStorage.hasBeenHereBefore === true || (location.search.match(/fast/) != null)) {
       document.body.className += ' has-been-here-before';
     }
-    document.body.className += ' trigger-pageload-animations';
-    return setTimeout((function() {
-      return document.body.className += ' trigger-pageload-animations-3s';
-    }), 3000);
-  };
+  }
 
-  window.home = home;
+  document.body.className += ' trigger-pageload-animations';
+
+  setTimeout((function() {
+    return document.body.className += ' trigger-pageload-animations-3s';
+  }), 3000);
 
 }).call(this);
